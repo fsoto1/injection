988380c63f6230b860c2b8321f11012d2f9266b3
Franco <f.soto@outlook.com>
2017-04-18 02:27:01 -0300

    agregado el diseño

--- pais.php
+++ pais.php
@@ -6,47 +6,51 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<div class="header"> 
-	<h1><a href="index.html">Buscador de paises</a></h1>
+<div class="clearfix">
+    <div class="header"> 
+    	<h1><a href="index.html">Buscador de paises</a></h1>
+    </div>
+    <div class="sidebar"> 
+    	<ul>
+    		<li> <a href="index.html">Buscar paises</a></li>
+    		<li> <a href="#">Buscar ciudades</a></li>
+    		<li> <a href="#">Buscar idioma nativo de paises</a></li>
+    	</ul>
+    </div>
+    <div class="content">
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $db = "world";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $db);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            echo "<p>Consulta: SELECT Code, Name, Continent FROM country WHERE Name LIKE '%".$_GET["pais"]."%' </p>" ;
+            $sql = "SELECT Code, Name, Continent FROM country WHERE Name LIKE '%".$_GET["pais"]."%' ";
+            $result = $conn->query($sql);
+
+
+
+            if ($result->num_rows > 0) {
+                echo "<table> <tr> <th>ID</th> <th>Nombre</th> <th>Contiente</th> </tr> ";
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr> <td>".$row["Code"]."</td> <td>".$row["Name"]."</td> <td>".$row["Continent"]."</td> </tr>";
+                }
+                echo "</table>";
+                } else {
+                    echo "No hay resultados";
+            }
+        ?> 
+    </div>
+    <div class="footer">
+        
+    </div>	
 </div>
-<div class="menu"> 
-	<ul>
-		<li class="active"> <a href="index.html">Buscar paises</a></li>
-		<li> <a href="#">Buscar ciudades</a></li>
-		<li> <a href="#">Buscar idioma nativo de paises</a></li>
-	</ul>
-</div>
-<div class="content">
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "world";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $db);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "<p> SELECT Code, Name, Continent FROM country WHERE Name LIKE '%".$_GET["pais"]."%' </p>" ;
-    $sql = "SELECT Code, Name, Continent FROM country WHERE Name LIKE '%".$_GET["pais"]."%' ";
-    $result = $conn->query($sql);
-
-
-
-    if ($result->num_rows > 0) {
-        echo "<table> <tr> <th>ID</th> <th>Nombre</th> <th>Contiente</th> </tr> ";
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            echo "<tr> <td>".$row["Code"]."</td> <td>".$row["Name"]."</td> <td>".$row["Continent"]."</td> </tr>";
-        }
-        echo "</table>";
-        } else {
-            echo "0 results";
-    }
-?> 
-</div>
-	
 </body>
 </html>
